bdce60b81e56ed6cdbc72ff0bfd6e1ca15b1f79c
Mubashir Ali <mubashir@botmd.io>
2023-03-15 00:26:31 +0500

    Add input field
    Add option for user to add phone number

---
+++ src/App.svelte
@@ -0,0 +1,28 @@
+<script>
+  let phoneInput = "92";
+  $: phoneHref = `https://api.whatsapp.com/send?phone=${phoneInput}`;
+</script>
+
+<main>
+  <h1>Direct WhatsApp</h1>
+  <p>
+    Enter phone number to directly send whatsapp message. Dont forget to add
+    country code.
+  </p>
+  <input bind:value={phoneInput} type="tel" />
+
+  <p>📲 Click on link to chat 🟢</p>
+  <p>
+    <a href={phoneHref}>+{phoneInput}</a>
+  </p>
+</main>
+
+<style>
+  input {
+    width: 90%;
+    height: 1.5rem;
+    text-align: center;
+    font-weight: 400;
+    font-size: 1.1em;
+  }
+</style>
